6e1599c8adbcdc767ea55f8a89a8da0cb7aa59a9
viedell <49792489+viedell@users.noreply.github.com>
2025-03-02 21:00:21 +0700

    numberfour.py

--- numberfour.py
+++ numberfour.py
@@ -1,6 +1,6 @@
 def apply_discount():
     price = int(input("Enter original price: "))
-    print("Discounted Price:", price * 0.7)
+    print("Discounted Price:", price * 0.8)
 
 if __name__ == "__main__":
     apply_discount()
